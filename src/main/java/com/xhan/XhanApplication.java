ee8e5020b837c831fa5c69dadebe537e727f4e85
xhan <960221962@qq.com>
2018-05-12 18:23:12 +0800

    Initial commit

---
+++ src/main/java/com/xhan/XhanApplication.java
@@ -0,0 +1,12 @@
+package com.xhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XhanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xhan/XhanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xhan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XhanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
